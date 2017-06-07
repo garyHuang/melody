e313dfcabd87f4ef7a8506cbf5820e578d80796c
hou <834865081@qq.com>
2017-06-07 18:07:01 +0800

    新增页面数据

--- src/main/java/hp/edu/orm/mapping/UsersSqlProvider.java
+++ src/main/java/hp/edu/orm/mapping/UsersSqlProvider.java
@@ -1,5 +1,6 @@
 package hp.edu.orm.mapping;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.jdbc.SQL;
 
 import hp.edu.orm.domain.Users;
@@ -11,6 +12,12 @@ public class UsersSqlProvider {
 		 sql.SELECT("id","username","password","name","email","mobile_phone","create_time","remarks","user_type","portrait_url","enabled");
 		 sql.FROM("users");
 		 if(null != users.getId()){
+			 sql.AND().WHERE("id=#{id}");
+		 }
+		 if( StringUtils.isNotBlank(users.getUsername())){
+			 sql.AND().WHERE("username=#{username}"); 
+		 }
+		 if( StringUtils.isNotBlank(users.getName())){
 			 sql.AND().WHERE("id=#{id}");
 		 }
 		 return sql.toString() ; 
