187a2812dc917e004e9dde266ff5f07035fc048f
garyHuang <835865081@qq.com>
2017-06-07 22:54:53 +0800

    up

--- src/main/java/hp/edu/service/UsersService.java
+++ src/main/java/hp/edu/service/UsersService.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -13,6 +14,7 @@ import hp.edu.orm.domain.Users;
 import hp.edu.orm.mapping.UsersMappping;
 
 @Service
+@Transactional
 public class UsersService {
 
 	@Autowired
