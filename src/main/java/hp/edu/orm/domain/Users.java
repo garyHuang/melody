785301f9d084797cc8c797786e7aa21beaac2864
GaryHuang <834865081@qq.com>
2017-06-08 17:59:39 +0800

    更新引擎代码

--- src/main/java/hp/edu/orm/domain/Users.java
+++ src/main/java/hp/edu/orm/domain/Users.java
@@ -5,76 +5,50 @@ package hp.edu.orm.domain;
 public class Users {
 	
 	/*
-	* ID
-	*/
-	private String id;
-	/*
-	* 用户名
-	*/
-	private String username;
-	/*
 	* 登录密码
 	*/
 	private String password;
-	/*
-	* 昵称
-	*/
-	private String name;
-	/*
-	* 邮箱
-	*/
-	private String email;
-	/*
-	* 手机号
-	*/
-	private String mobilePhone;
-	/*
-	* 创建时间
-	*/
-	private java.sql.Timestamp createTime;
-	/*
-	* 备注
-	*/
-	private String remarks;
 	/*
 	* 用户类型
 	*/
 	private String userType;
 	/*
+	* 创建时间
+	*/
+	private java.sql.Timestamp createTime;
+	/*
+	* 手机号
+	*/
+	private String mobilePhone;
+	/*
+	* 昵称
+	*/
+	private String name;
+	/*
+	* ID
+	*/
+	private String id;
+	/*
 	* 图片url
 	*/
 	private String portraitUrl;
 	/*
+	* 邮箱
+	*/
+	private String email;
+	/*
+	* 备注
+	*/
+	private String remarks;
+	/*
 	* 是否启用
 	*/
 	private Boolean enabled;
-	
-	
-	/*
-	* ID
-	*/
-	public void setId(String id){
-		this.id = id ;
-	}
-	/*
-	* ID
-	*/
-	public String getId(){
-		return this.id ; 
-	}
-	
 	/*
 	* 用户名
 	*/
-	public void setUsername(String username){
-		this.username = username ;
-	}
-	/*
-	* 用户名
-	*/
-	public String getUsername(){
-		return this.username ; 
-	}
+	private String username;
+	
 	
 	/*
 	* 登录密码
@@ -87,71 +61,6 @@ public class Users {
 	*/
 	public String getPassword(){
 		return this.password ; 
-	}
-	
-	/*
-	* 昵称
-	*/
-	public void setName(String name){
-		this.name = name ;
-	}
-	/*
-	* 昵称
-	*/
-	public String getName(){
-		return this.name ; 
-	}
-	
-	/*
-	* 邮箱
-	*/
-	public void setEmail(String email){
-		this.email = email ;
-	}
-	/*
-	* 邮箱
-	*/
-	public String getEmail(){
-		return this.email ; 
-	}
-	
-	/*
-	* 手机号
-	*/
-	public void setMobilePhone(String mobilePhone){
-		this.mobilePhone = mobilePhone ;
-	}
-	/*
-	* 手机号
-	*/
-	public String getMobilePhone(){
-		return this.mobilePhone ; 
-	}
-	
-	/*
-	* 创建时间
-	*/
-	public void setCreateTime(java.sql.Timestamp createTime){
-		this.createTime = createTime ;
-	}
-	/*
-	* 创建时间
-	*/
-	public java.sql.Timestamp getCreateTime(){
-		return this.createTime ; 
-	}
-	
-	/*
-	* 备注
-	*/
-	public void setRemarks(String remarks){
-		this.remarks = remarks ;
-	}
-	/*
-	* 备注
-	*/
-	public String getRemarks(){
-		return this.remarks ; 
 	}
 	
 	/*
@@ -168,6 +77,58 @@ public class Users {
 	}
 	
 	/*
+	* 创建时间
+	*/
+	public void setCreateTime(java.sql.Timestamp createTime){
+		this.createTime = createTime ;
+	}
+	/*
+	* 创建时间
+	*/
+	public java.sql.Timestamp getCreateTime(){
+		return this.createTime ; 
+	}
+	
+	/*
+	* 手机号
+	*/
+	public void setMobilePhone(String mobilePhone){
+		this.mobilePhone = mobilePhone ;
+	}
+	/*
+	* 手机号
+	*/
+	public String getMobilePhone(){
+		return this.mobilePhone ; 
+	}
+	
+	/*
+	* 昵称
+	*/
+	public void setName(String name){
+		this.name = name ;
+	}
+	/*
+	* 昵称
+	*/
+	public String getName(){
+		return this.name ; 
+	}
+	
+	/*
+	* ID
+	*/
+	public void setId(String id){
+		this.id = id ;
+	}
+	/*
+	* ID
+	*/
+	public String getId(){
+		return this.id ; 
+	}
+	
+	/*
 	* 图片url
 	*/
 	public void setPortraitUrl(String portraitUrl){
@@ -181,6 +142,32 @@ public class Users {
 	}
 	
 	/*
+	* 邮箱
+	*/
+	public void setEmail(String email){
+		this.email = email ;
+	}
+	/*
+	* 邮箱
+	*/
+	public String getEmail(){
+		return this.email ; 
+	}
+	
+	/*
+	* 备注
+	*/
+	public void setRemarks(String remarks){
+		this.remarks = remarks ;
+	}
+	/*
+	* 备注
+	*/
+	public String getRemarks(){
+		return this.remarks ; 
+	}
+	
+	/*
 	* 是否启用
 	*/
 	public void setEnabled(Boolean enabled){
@@ -191,5 +178,18 @@ public class Users {
 	*/
 	public Boolean isEnabled(){
 		return this.enabled ; 
+	}
+	
+	/*
+	* 用户名
+	*/
+	public void setUsername(String username){
+		this.username = username ;
+	}
+	/*
+	* 用户名
+	*/
+	public String getUsername(){
+		return this.username ; 
 	}
 }
--- src/main/java/hp/edu/orm/mapping/UsersMappping.java
+++ src/main/java/hp/edu/orm/mapping/UsersMappping.java
@@ -15,49 +15,53 @@ import org.apache.ibatis.type.JdbcType;
 import hp.edu.orm.domain.Users;
 
 public interface UsersMappping {
+
 	@Delete({ "delete from users", "where id = #{id,jdbcType=VARCHAR}" })
 	int deleteByPrimaryKey(String id);
 
 	@Insert({
-			"insert into users (id,username,password,name,email,mobile_phone,create_time,remarks,user_type,portrait_url,enabled)",
-			"values (#{id,jdbcType=VARCHAR},#{username,jdbcType=VARCHAR},#{password,jdbcType=VARCHAR},#{name,jdbcType=VARCHAR},#{email,jdbcType=VARCHAR},#{mobilePhone,jdbcType=VARCHAR},#{createTime,jdbcType=TIMESTAMP},#{remarks,jdbcType=VARCHAR},#{userType,jdbcType=CHAR},#{portraitUrl,jdbcType=VARCHAR},#{enabled,jdbcType=BIT})" })
+			"insert into users (password,user_type,create_time,mobile_phone,name,id,portrait_url,email,remarks,enabled,username)",
+			"values (#{password,jdbcType=VARCHAR},#{userType,jdbcType=VARCHAR},#{createTime,jdbcType=TIMESTAMP},#{mobilePhone,jdbcType=VARCHAR},#{name,jdbcType=VARCHAR},#{id,jdbcType=VARCHAR},#{portraitUrl,jdbcType=VARCHAR},#{email,jdbcType=VARCHAR},#{remarks,jdbcType=VARCHAR},#{enabled,jdbcType=BIT},#{username,jdbcType=VARCHAR})" })
 	int insert(Users users);
 
 	@Select({ "select",
-			"id,username,password,name,email,mobile_phone,create_time,remarks,user_type,portrait_url,enabled",
+			"password,user_type,create_time,mobile_phone,name,id,portrait_url,email,remarks,enabled,username",
 			"from users" })
-	@Results({ @Result(column = "id", property = "id", jdbcType = JdbcType.VARCHAR, id = true),
-			@Result(column = "username", property = "username", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "name", property = "name", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "mobile_phone", property = "mobilePhone", jdbcType = JdbcType.VARCHAR),
+	@Results({ @Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "user_type", property = "userType", jdbcType = JdbcType.VARCHAR),
 			@Result(column = "create_time", property = "createTime", jdbcType = JdbcType.TIMESTAMP),
-			@Result(column = "remarks", property = "remarks", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "user_type", property = "userType", jdbcType = JdbcType.CHAR),
+			@Result(column = "mobile_phone", property = "mobilePhone", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "name", property = "name", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "id", property = "id", jdbcType = JdbcType.VARCHAR, id = true),
 			@Result(column = "portrait_url", property = "portraitUrl", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "enabled", property = "enabled", jdbcType = JdbcType.BIT)
+			@Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "remarks", property = "remarks", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "enabled", property = "enabled", jdbcType = JdbcType.BIT),
+			@Result(column = "username", property = "username", jdbcType = JdbcType.VARCHAR)
 
 	})
 	List<Users> selectAll();
 
 	@Select({ "select",
-			"id,username,password,name,email,mobile_phone,create_time,remarks,user_type,portrait_url,enabled",
+			"password,user_type,create_time,mobile_phone,name,id,portrait_url,email,remarks,enabled,username",
 			"from users where id=#{id,jdbcType=VARCHAR}" })
-	@Results({ @Result(column = "id", property = "id", jdbcType = JdbcType.VARCHAR, id = true),
-			@Result(column = "username", property = "username", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "name", property = "name", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "mobile_phone", property = "mobilePhone", jdbcType = JdbcType.VARCHAR),
+	@Results({ @Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "user_type", property = "userType", jdbcType = JdbcType.VARCHAR),
 			@Result(column = "create_time", property = "createTime", jdbcType = JdbcType.TIMESTAMP),
-			@Result(column = "remarks", property = "remarks", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "user_type", property = "userType", jdbcType = JdbcType.CHAR),
+			@Result(column = "mobile_phone", property = "mobilePhone", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "name", property = "name", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "id", property = "id", jdbcType = JdbcType.VARCHAR, id = true),
 			@Result(column = "portrait_url", property = "portraitUrl", jdbcType = JdbcType.VARCHAR),
-			@Result(column = "enabled", property = "enabled", jdbcType = JdbcType.BIT)
+			@Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "remarks", property = "remarks", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "enabled", property = "enabled", jdbcType = JdbcType.BIT),
+			@Result(column = "username", property = "username", jdbcType = JdbcType.VARCHAR)
 
 	})
 	Users selectByPrimaryKey(String id);
+
+	@Update({ "update users", "set id = #{id,jdbcType=VARCHAR},", "where id = #{id,jdbcType=VARCHAR}" })
+	int updateByPrimaryKey(Users users);
 
 	@SelectProvider(method = "selectByAny", type = UsersSqlProvider.class)
 	@Results({ @Result(column = "id", property = "id", jdbcType = JdbcType.VARCHAR, id = true),
@@ -75,11 +79,6 @@ public interface UsersMappping {
 	})
 	List<Users> selectByAny(Users users);
 
-	@Update({ "update users",
-			"set username = #{username,jdbcType=VARCHAR},password = #{password,jdbcType=VARCHAR},name = #{name,jdbcType=VARCHAR},email = #{email,jdbcType=VARCHAR},mobile_phone = #{mobilePhone,jdbcType=VARCHAR},create_time = #{createTime,jdbcType=TIMESTAMP},remarks = #{remarks,jdbcType=VARCHAR},user_type = #{userType,jdbcType=CHAR},portrait_url = #{portraitUrl,jdbcType=VARCHAR},enabled = #{enabled,jdbcType=BIT}",
-			"where id = #{id,jdbcType=VARCHAR}" })
-	int updateByPrimaryKey(Users users);
-	
 	@SelectProvider(method = "selectByAny", type = UsersSqlProvider.class)
-	List<Map<String,Object>> selectMaps(Users users);
+	List<Map<String, Object>> selectMaps(Users users);
 }
